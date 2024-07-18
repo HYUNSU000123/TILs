afe4e405065d1756dea274fd20e7a23545719c1c
SU <130213299+HYUNSU000123@users.noreply.github.com>
2024-07-19 00:26:40 +0900

    Title: 굉장한 숫자, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240719/굉장한 숫자/amazing-number.java
+++ 240719/굉장한 숫자/amazing-number.java
@@ -6,5 +6,7 @@ public class Main {
         int n = sc.nextInt();
         if ((n % 2 != 0 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0))
             System.out.print("true");
+        else
+            System.out.print("false");
     }
 }
