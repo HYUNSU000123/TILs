9967dc39955c18ca97cd004848905b43b61ed06e
SU <130213299+HYUNSU000123@users.noreply.github.com>
2024-07-19 04:23:54 +0900

    Title: 코로나 메뉴얼, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240719/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String aTmp = sc.next();
+        int aBdy = sc.nextInt();
+        String bTmp = sc.next();
+        int bBdy = sc.nextInt();
+        String cTmp = sc.next();
+        int cBdy = sc.nextInt();
+
+        String result1 = "";
+        String result2 = "";
+        String result3 = "";
+
+        if (aTmp.equals("Y") && aBdy >= 37) {
+            result1 = "A";
+        } else if (aTmp.equals("N") && aBdy >= 37) {
+            result1 = "B";
+        } else {
+            result1 = "C";
+        }
+
+        if (bTmp.equals("Y") && aBdy >= 37) {
+            result2 = "A";
+        } else if (bTmp.equals("N") && bBdy >= 37) {
+            result2 = "B";
+        } else {
+            result3 = "C";
+        }
+
+        if (cTmp.equals("Y") && cBdy >= 37) {
+            result3 = "A";
+        } else if (cTmp.equals("N") && cBdy >= 37) {
+            result3 = "B";
+        } else {
+            result3 = "C";
+        }
+
+        int n = 0;
+
+        if (result1.equals("A"))
+            n++;
+        if (result2.equals("A"))
+            n++;
+        if (result3.equals("A"))
+            n++;
+
+        if (n >= 2) {
+            System.out.print("E");
+        }
+        else {
+            System.out.print("N");
+        }
+    }
+}
