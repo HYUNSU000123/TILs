a96b8b03923d3df6e87c520d61aa89da8fb82d1f
SU <130213299+HYUNSU000123@users.noreply.github.com>
2024-07-18 16:11:18 +0900

    Title: 살 수 있는 물건, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240718/살 수 있는 물건/things-able-to-buy.java
+++ 240718/살 수 있는 물건/things-able-to-buy.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n >= 3000) {
+        if (n == 3000) {
             System.out.print("book");
         }
-        else if (n >= 1000) {
+        else if (n == 1000) {
             System.out.print("mask");
         }
     }
