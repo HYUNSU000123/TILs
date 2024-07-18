e84741aff6a747c3ba6d47a6bbc7acb2be86f5f4
SU <130213299+HYUNSU000123@users.noreply.github.com>
2024-07-19 00:36:10 +0900

    Title: 숫자의 계절은, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/숫자의 계절은/season-of-num.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        if (month >= 3 && month <= 5) {
+            System.out.print("Spring");
+        }
+        else if (month >= 6 && month <= 8) {
+            System.out.print("Summer");
+        }
+        else if (month >= 9 && month <= 11) {
+            System.out.print("Fall");
+        }
+        else if (month <= 2 || month > 11) {
+            System.out.print("Winter");
+        }
+    }
+}
