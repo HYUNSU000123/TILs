2d9513c880cef681d70c11eff6f14d4d1a2cdd4c
SU <130213299+HYUNSU000123@users.noreply.github.com>
2024-07-19 03:47:01 +0900

    Title: 일 수 구하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240719/일 수 구하기/number-of-days-in-month.java
+++ 240719/일 수 구하기/number-of-days-in-month.java
@@ -8,10 +8,12 @@ public class Main {
             System.out.print("28");
         }
         else {
-            if (n % 2 == 1)
+            if (n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12) {
                 System.out.print("31");
-            else if (n % 2 == 0)
+            }
+            else {
                 System.out.print("30");
+            }
         }
         
     }
